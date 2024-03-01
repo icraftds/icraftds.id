126b6590c97c82218ea9105adcfcdbbb330f3be1
Seya <31957516+sglkc@users.noreply.github.com>
2024-03-01 21:39:18 +0700

    feat(Hero): update layout

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,34 +1,29 @@
-import { useLenis } from '@studio-freight/react-lenis'
-import { m, useInView } from 'framer-motion'
-import { useRef } from 'preact/hooks'
-
 export default function About() {
-  const lenis = useLenis()
-  const ref = useRef(null)
-  const inView = useInView(ref, { amount: 1 })
-  const onClick = () => !inView && lenis.scrollTo('#about', { duration: 2 })
-
   return (
-    <div class="-mt-16">
-      <m.div
-        class="relative h-16 cursor-pointer bg-black rounded-t-16"
-        style={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        onClick={onClick}
-      />
-      <m.section
-        ref={ref}
-        id="about"
-        class="relative px-8 py-32 h-[100svh] bg-black"
+    <section
+      id="about"
+      class="mx-auto my-16 px-16 pt-16 pb-32 max-w-7xl grid grid-cols-8 gap-16 bg-gray-100 rounded-xl"
+    >
+      <h2
+        class="font-sans text-4xl fw-medium col-span-full tracking-wider flex gap-8"
       >
-        <p class="m-auto text-center max-w-2xl font-serif text-white text-5xl leading-14">
+        <span>&gt;</span>
+        <span><em>About</em></span>
+        <span class="relative w-32 h-0.5 bg-black self-center" />
+      </h2>
+      <div
+        class="flex flex-col gap-8 col-start-4 col-span-full text-2xl font-sans leading-12"
+      >
+        <p>
           Kami adalah perusahaan IT yang terpusat di Kota Bandung, berdiri sejak
-          tanggal 14 Januari 2023 dengan melayani layanan digital berupa pembuatan
+          tanggal 14 Januari 2023.
+        </p>
+        <p>
+          Dengan melayani layanan digital berupa pembuatan
           aplikasi perangkat lunak yang terintegrasi sehingga membuat proses bisnis
           menjadi mudah.
         </p>
-      </m.section>
-    </div>
+      </div>
+    </section>
   )
 }
