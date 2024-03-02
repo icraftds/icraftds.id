2cbce750c4ec7e974d97c6fddeabae8dedec90f5
Seya <31957516+sglkc@users.noreply.github.com>
2024-03-02 09:01:32 +0700

    feat(Commitment): semantic html tag

--- src/components/Commitment.jsx
+++ src/components/Commitment.jsx
@@ -1,11 +1,11 @@
 export default function Commitment({ i, title, text }) {
   return (
-    <li class="flex gap-8">
+    <li class="flex items-center gap-12">
       <p class="font-serif text-4xl">{ i }</p>
-      <div class="">
-        <h3 class="font-sans text-2xl fw-medium">{ title }</h3>
-        <p>{ text }</p>
-      </div>
+      <figure>
+        <figcaption class="font-sans text-2xl fw-medium">{ title }</figcaption>
+        <p class="mt-2">{ text }</p>
+      </figure>
     </li>
   )
 }
