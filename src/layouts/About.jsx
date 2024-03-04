1aa7d5df8ec301044658647dd10124731b744853
Seya <31957516+sglkc@users.noreply.github.com>
2024-03-04 21:04:33 +0700

    feat(About): responsif

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,19 +1,27 @@
+import clsx from "clsx";
+
 export default function About() {
   return (
     <section
       id="about"
-      class="mt-16 px-16 py-32 bg-black text-gray-100"
+      class="mt-16 px-8 py-32 sm:px-16 bg-black text-gray-100"
     >
-      <div class="mx-auto max-w-7xl grid grid-cols-8 gap-16">
+      <div class="mx-auto max-w-4xl grid grid-cols-8 gap-8">
         <h2
-          class="text-4xl fw-medium col-span-full tracking-wider flex gap-8"
+          class={clsx(
+            "text-4xl fw-medium col-span-full tracking-wider flex gap-8",
+            "sm:text-5xl"
+          )}
         >
           <span>&gt;</span>
           <span><em>About</em></span>
-          <span class="relative w-32 h-0.5 bg-black self-center" />
         </h2>
         <div
-          class="flex flex-col gap-8 col-start-4 col-span-full text-2xl leading-12"
+          class={clsx(
+            "flex flex-col gap-8 col-span-full text-xl leading-10",
+            "sm:col-start-2 md:col-start-3 lg:col-start-4",
+            "sm:(text-2xl leading-12)"
+          )}
         >
           <p>
             Kami adalah perusahaan IT yang terpusat di Kota Bandung, berdiri sejak
