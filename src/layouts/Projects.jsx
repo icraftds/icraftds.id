a20a83fc5c940c442c32ce38ab8fe08cc3a8162c
Seya <31957516+sglkc@users.noreply.github.com>
2024-03-04 19:59:05 +0700

    feat: tambah proyek simak monitoring

--- src/layouts/Projects.jsx
+++ src/layouts/Projects.jsx
@@ -3,21 +3,23 @@ import Project from "@/components/Project"
 export default function Projects() {
   const projects = [
     {
-      i: 1,
       name: 'Hubin 13',
       src: 'hubin-13.webp',
       desc: 'Aplikasi web untuk membantu dalam pemetaan praktik kerja lapangan'
     },
     {
-      i: 2,
-      name: 'Simak PPDB',
-      src: 'simak-ppdb.webp',
+      name: 'svknd.id',
+      src: 'savikindo.webp',
       desc: 'Landing page perusahaan PT. Satu Visi Kreasi Indonesia'
     },
     {
-      i: 3,
-      name: 'svknd.id',
-      src: 'savikindo.webp',
+      name: 'Simak PPDB',
+      src: 'simak-ppdb.webp',
+      desc: 'Landing page produk PT. Satu Visi Kreasi Indonesia'
+    },
+    {
+      name: 'Simak Monitoring',
+      src: 'simak-monitoring.webp',
       desc: 'Landing page produk PT. Satu Visi Kreasi Indonesia'
     },
   ]
@@ -27,7 +29,7 @@ export default function Projects() {
       <div class="mx-auto max-w-6xl grid gap-16">
         <h2 class="fw-bold text-6xl">Proyek</h2>
         <ul class="grid grid-cols-2 gap-8">
-          { projects.map((project) => <Project key={project.i} {...project} />)}
+          { projects.map((project, i) => <Project key={i} i={i+1} {...project} />)}
         </ul>
       </div>
     </section>
